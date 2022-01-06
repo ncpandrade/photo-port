f57645fbb3ed8558b1cb6fe7c7f7704d2e12e934
Nadine <nadine_paez@hotmail.com>
2022-01-05 19:46:58 -0500

    test about

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -4,6 +4,7 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
+//ensure that after each test, we won't have any leftover memory
 afterEach(cleanup);
 
 describe('About component', () => {
